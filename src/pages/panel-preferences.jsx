c0ba6ab769b81068f5f043fd6bade20face340dd
Rage Brahimi <github@rage.al>
2020-12-10 12:20:59 +0000

    Adding Switch Theme Color option.
    Enable the option to select theme color, amongst theme mode.
    Just gives another personal touch to the PWA.
    
    Changes:
     - New function added 'setThemeColor'
     - Added new smart select option for color switch

--- src/pages/panel-preferences.jsx
+++ src/pages/panel-preferences.jsx
@@ -1,14 +1,22 @@
-import React, { useRef } from 'react';
+import React from 'react';
 import { f7, Navbar, Page, List, ListItem } from 'framework7-react';
 
 export default () => {
-  const preferences = useRef(null);
-
   const setThemeMode = () => {
-    preferences.current = f7.form.convertToData('#preferences');
-    f7.$('html')
-      .removeClass('theme-dark theme-light')
-      .addClass(`theme-${preferences.current.theme}`);
+    const $html = f7.$('html');
+    const preferences = f7.form.convertToData('#preferences');
+
+    $html.removeClass('theme-dark theme-light').addClass(`theme-${preferences.theme}`);
+  };
+
+  const setThemeColor = () => {
+    const $html = f7.$('html');
+    const preferences = f7.form.convertToData('#preferences');
+
+    const currentColorClass = $html[0].className.match(/color-theme-([a-z]*)/);
+    if (currentColorClass) $html.removeClass(currentColorClass[0]);
+
+    $html.addClass(`color-theme-${preferences.color}`);
   };
 
   return (
@@ -22,6 +30,14 @@ export default () => {
           <select name="theme" onChange={setThemeMode}>
             <option value="dark">Dark</option>
             <option value="light">Light</option>
+          </select>
+        </ListItem>
+        {/* Switch Theme Color */}
+        <ListItem title="Color" smartSelect smartSelectInit>
+          <select name="color" onChange={setThemeColor}>
+            <option value="green">Green</option>
+            <option value="blue">Blue</option>
+            <option value="red">Red</option>
           </select>
         </ListItem>
       </List>
