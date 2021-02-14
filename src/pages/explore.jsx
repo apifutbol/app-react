d6e2165492e6d0d22f00802ddf9f0c25b41e1952
Rage Brahimi <github@rage.al>
2021-02-14 16:30:55 +0000

    Add Pull to Refresh example to the 'Explore' tab.
    Changes:
     - Updated the 'Explore' tab to include Pull to Refresh

--- src/pages/explore.jsx
+++ src/pages/explore.jsx
@@ -1,13 +1,76 @@
-import React from 'react';
-import { Page, Block } from 'framework7-react';
+import React, { useState } from 'react';
+import { Page, List, ListItem, SkeletonBlock } from 'framework7-react';
 
 export default () => {
+  const [loading, setLoading] = useState(false);
+
+  const ptr = (done) => {
+    setLoading(true);
+
+    setTimeout(() => {
+      setLoading(false);
+
+      done();
+    }, 1000);
+  };
+
   return (
-    <Page pageContent={true}>
+    <Page pageContent={true} ptr ptrMousewheel={true} onPtrRefresh={ptr}>
       {/* Page Content */}
-      <Block strong>
-        <p>Explore</p>
-      </Block>
+      {loading ? (
+        <List mediaList v-if="loading">
+          {[1, 2, 3].map((n) => (
+            <ListItem
+              key={n}
+              className="skeleton-text skeleton-effect-fade"
+              title="Full Name"
+              subtitle="Position"
+              text="Lorem ipsum dolor sit amet, consectetur adipiscing elit. Morbi lobortis et massa ac interdum. Cras consequat felis at consequat hendrerit. Aliquam vestibulum vitae lorem ac iaculis. Praesent nec pharetra massa, at blandit lectus. Sed tincidunt, lectus eu convallis elementum, nibh nisi aliquet urna, nec imperdiet felis sapien at enim."
+            >
+              <SkeletonBlock
+                style={{ width: '40px', height: '40px', borderRadius: '50%' }}
+                slot="media"
+              />
+            </ListItem>
+          ))}
+        </List>
+      ) : (
+        <List mediaList>
+          <ListItem
+            title="John Doe"
+            subtitle="CEO"
+            text="Lorem ipsum dolor sit amet, consectetur adipiscing elit. Morbi lobortis et massa ac interdum. Cras consequat felis at consequat hendrerit. Aliquam vestibulum vitae lorem ac iaculis. Praesent nec pharetra massa, at blandit lectus. Sed tincidunt, lectus eu convallis elementum, nibh nisi aliquet urna, nec imperdiet felis sapien at enim."
+          >
+            <img
+              src="https://placeimg.com/80/80/people/1"
+              style={{ width: '40px', height: '40px', borderRadius: '50%' }}
+              slot="media"
+            />
+          </ListItem>
+          <ListItem
+            title="Jane Doe"
+            subtitle="Marketing"
+            text="Cras consequat felis at consequat hendrerit. Aliquam vestibulum vitae lorem ac iaculis. Praesent nec pharetra massa, at blandit lectus. Sed tincidunt, lectus eu convallis elementum, nibh nisi aliquet urna, nec imperdiet felis sapien at enim."
+          >
+            <img
+              src="https://placeimg.com/80/80/people/2"
+              style={{ width: '40px', height: '40px', borderRadius: '50%' }}
+              slot="media"
+            />
+          </ListItem>
+          <ListItem
+            title="Kate Johnson"
+            subtitle="Admin"
+            text="Sed tincidunt, lectus eu convallis elementum, nibh nisi aliquet urna, nec imperdiet felis sapien at enim."
+          >
+            <img
+              src="https://placeimg.com/80/80/people/3"
+              style={{ width: '40px', height: '40px', borderRadius: '50%' }}
+              slot="media"
+            />
+          </ListItem>
+        </List>
+      )}
     </Page>
   );
 };
