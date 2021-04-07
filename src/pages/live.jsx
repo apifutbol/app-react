ae71b6715d769a3cb2c1e5ee5c392b378f6e7563
Rage Brahimi <github@rage.al>
2021-04-07 15:12:46 +0100

    Add Navbar to Live

--- src/pages/live.jsx
+++ src/pages/live.jsx
@@ -1,12 +1,14 @@
 import React from 'react';
-import { Page, Block } from 'framework7-react';
+import { Page, Navbar, Block } from 'framework7-react';
 
 export default () => {
   return (
-    <Page pageContent={true}>
+    <Page name="live" pageContent={true}>
+      {/* Top Navbar */}
+      <Navbar title="Live" />
       {/* Page Content */}
       <Block strong>
-        <p>API Futbol React App</p>
+        <p>API Futbol Live</p>
       </Block>
     </Page>
   );
