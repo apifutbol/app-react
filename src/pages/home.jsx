74cd15b02fbbac1730e1ee448a433d06febacb19
Rage Brahimi <github@rage.al>
2021-04-07 15:13:47 +0100

    Remove tabs from Home. Now using views.

--- src/pages/home.jsx
+++ src/pages/home.jsx
@@ -1,36 +1,15 @@
 import React from 'react';
-import { Page, Navbar, NavTitle, Toolbar, Link, Tabs, Tab } from 'framework7-react';
+import { Page, Navbar, Block } from 'framework7-react';
 
 export default () => {
   return (
-    <Page name="home" pageContent={false}>
+    <Page name="home" pageContent={true}>
       {/* Top Navbar */}
-      <Navbar>
-        <NavTitle>API Futbol</NavTitle>
-      </Navbar>
-      {/* Bottom Toolbar */}
-      <Toolbar bottom tabbar>
-        <Link
-          tabLink
-          href="/"
-          routeTabId="live"
-          iconIos="f7:sportscourt"
-          iconMd="f7:sportscourt"
-          tabLinkActive
-        />
-        <Link
-          tabLink
-          href="/explore/"
-          routeTabId="explore"
-          iconIos="f7:search"
-          iconMd="f7:search"
-        />
-      </Toolbar>
-      {/* Routable Tabs */}
-      <Tabs routable swipeable>
-        <Tab id="live" tabActive />
-        <Tab id="explore" />
-      </Tabs>
+      <Navbar title="Home" />
+      {/* Page Content */}
+      <Block strong>
+        <p>API Futbol Home</p>
+      </Block>
     </Page>
   );
 };
